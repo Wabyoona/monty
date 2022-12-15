1c8bfb3cff43e13f457ca672516ce96c9545fda9
Wabyoona <maxwell@gmail.com>
2022-12-15 12:53:38 -0800

    pchar.c

--- pchar.c
+++ pchar.c
@@ -1,43 +1,21 @@
 #include "monty.h"
-
 #include <ctype.h>
-
-
-
 /**
- *
- *  * m_pchar - print character from top of stack
- *
- *   * @stack: double pointer to top of stack
- *
- *    * @line_number: line number of current operation
- *
- *     *
- *
- *      * Return: void
- *
- *       */
+ * m_pchar - print character from top of stack
+ * @stack: double pointer to top of stack
+ * @line_number: line number of current operation
+ * Return: void
+ */
 
 void m_pchar(stack_t **stack, unsigned int line_number)
-
 {
-
-		int ch;
-
-
-
-			if (var.stack_len < 1)
-
-					{
-
-								dprintf(STDOUT_FILENO,
-
-													"L%u: can't pchar, stack empty\n",
-
-																line_number);
-
-										exit(EXIT_FAILURE);
-
+int ch;
+if (var.stack_len < 1)
+{
+dprintf(STDOUT_FILENO,
+"L%u: can't pchar, stack empty\n",
+line_number);
+exit(EXIT_FAILURE);
 }
 ch = (*stack)->n;
 if (!isascii(ch))
