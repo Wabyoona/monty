a6b22a86ad924cce4e013499b78137250e1c77ef
Wabyoona <maxwell@gmail.com>
2022-12-15 08:30:28 -0800

    pint.c

--- pint.c
+++ pint.c
@@ -1,16 +1,15 @@
 #include "monty.h"
+
 /**
- * pint - prints the value at the top of the stack, followed by a new line.
- * @stack: double pointer to the head of the stack
- * @line_number: the number of the line in file
- * Return: void
+ * pint - Prints the value at the top of the stack,
+ * followed by a new line.
+ * @stack: The head of the stack
+ * @line_number: The line on which the error occurred
+ * Return: Nothing
  */
 void pint(stack_t **stack, unsigned int line_number)
 {
-if (stack == NULL || *stack == NULL)
-{
-printf("L%u: can't pint, stack empty\n", line_number);
-exit(EXIT_FAILURE);
-}
+if (*stack == NULL)
+handle_error(ERR_PINT_USG, NULL, line_number, NULL);
 printf("%d\n", (*stack)->n);
 }
