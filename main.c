534dd597f80f20dd2b77b1a87dbab17a4ffb2afb
Wabyoona <maxwell@gmail.com>
2022-12-15 11:51:59 -0800

    main.c

--- main.c
+++ main.c
@@ -1,48 +1,24 @@
 #include "monty.h"
 
-
-
 /* global struct to hold flag for queue and stack length */
-
 var_t var;
-
-
-
 /**
- *
- *  * main - Monty bytecode interpreter
- *
- *   * @argc: number of arguments passed
- *
- *    * @argv: array of argument strings
- *
- *     *
- *
- *      * Return: EXIT_SUCCESS on success or EXIT_FAILURE on failure
- *
- *       */
+ * main - Monty bytecode interpreter
+ * @argc: number of arguments passed
+ * @argv: array of argument strings
+ * Return: EXIT_SUCCESS on success or EXIT_FAILURE on failure
+ */
 
 int main(int argc, char *argv[])
-
 {
-
-		stack_t *stack = NULL;
-
-			unsigned int line_number = 0;
-
-				FILE *fs = NULL;
-
-					char *lineptr = NULL, *op = NULL;
-
-						size_t n = 0;
-
-
-
-							var.queue = 0;
-
-								var.stack_len = 0;
-
-									if (argc != 2)
+stack_t *stack = NULL;
+unsigned int line_number = 0;
+FILE *fs = NULL;
+char *lineptr = NULL, *op = NULL;
+size_t n = 0;
+var.queue = 0;
+var.stack_len = 0;
+if (argc != 2)
 {
 dprintf(STDOUT_FILENO, "USAGE: monty file\n");
 exit(EXIT_FAILURE);
