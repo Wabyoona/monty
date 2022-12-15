71f9e59bafa83e534e3269a4e9f719a321a95841
Wabyoona <maxwell@gmail.com>
2022-12-15 12:59:11 -0800

    pstr.c

--- pstr.c
+++ pstr.c
@@ -17,45 +17,22 @@
  *      *
  *
  *       * Return: void
- *
- *        */
-
+ */
 void m_pstr(stack_t **stack, unsigned int line_number)
-
 {
-
-		stack_t *temp;
-
-			int ch;
-
-
-
-				(void)line_number;
-
-
-
-					temp = *stack;
-
-						while (temp != NULL)
-
-								{
-
-											ch = temp->n;
-
-													if (!isascii(ch) || ch == 0)
-
-																	break;
-
-															putchar(ch);
-
-																	temp = temp->next;
-
-																			if (temp == *stack)
-
-																							break;
-
-																				}
-
-							putchar('\n');
-
+stack_t *temp;
+int ch;
+(void)line_number;
+temp = *stack;
+while (temp != NULL)
+{
+ch = temp->n;
+if (!isascii(ch) || ch == 0)
+break;
+putchar(ch);
+temp = temp->next;
+if (temp == *stack)
+break;
+}
+putchar('\n');
 }
